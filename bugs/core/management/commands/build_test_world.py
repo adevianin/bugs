6d29bdc459939a534e3beededff79ee805cf9891
Anton Devianin <adevianin@gmail.com>
2023-04-17 17:54:29 +0300

    2 bugs

--- bugs/core/management/commands/build_test_world.py
+++ bugs/core/management/commands/build_test_world.py
@@ -7,19 +7,22 @@ class Command(BaseCommand):
     world_state = {
         "map": { "size": {"width": 3000, "height": 3000 }},
         "bugs": [
-            { "id": 1, "position": {"x": 650, "y": 200}, "from_town": 30, "type": "food_collector" }
+            { "id": 1, "position": {"x": 650, "y": 200}, "from_town": 30, "type": "food_collector" },
+            { "id": 2, "position": {"x": 1600, "y": 350}, "from_town": 31, "type": "food_collector" },
         ],
         "foods": [
-            {"id": 20, "position": {"x": 350, "y": 200}, "calories": 150, "type": "nectar", "food_variety": 1 },
-            {"id": 21, "position": {"x": 850, "y": 200}, "calories": 300, "type": "nectar", "food_variety": 2  }
+            {"id": 20, "position": {"x": 350, "y": 200}, "calories": 150, "type": "leaf", "food_variety": 1 },
+            {"id": 21, "position": {"x": 850, "y": 200}, "calories": 300, "type": "leaf", "food_variety": 2  }
         ],
         "towns": [
-            { "id": 30, "position": {"x": 600, "y": 200 }, "color": "yellow" }
+            { "id": 30, "position": {"x": 600, "y": 200 }, "color": "yellow" },
+            { "id": 31, "position": {"x": 1600, "y": 300 }, "color": "red" },
         ],
         "food_areas": [
-            { "id": 40, "position": {"x": 450, "y": 150}, "size": { "width": 150, "height": 150 }, "fertility": 20, "food_type": "nectar" }
+            { "id": 40, "position": {"x": 450, "y": 150}, "size": { "width": 150, "height": 150 }, "fertility": 20, "food_type": "leaf" },
+            { "id": 41, "position": {"x": 1650, "y": 250}, "size": { "width": 150, "height": 150 }, "fertility": 20, "food_type": "nectar" },
         ],
-        "last_id": 40
+        "last_id": 41
     }
 
     def handle(self, *args, **options):
