ea460c8fd7b18afd841100e1cfa1eeab018f73f6
Anton Devianin <adevianin@gmail.com>
2023-06-19 21:11:20 +0300

    changes for test

--- bugs/core/world/entities/ant/worker/worker_ant_mind.py
+++ bugs/core/world/entities/ant/worker/worker_ant_mind.py
@@ -3,10 +3,11 @@ from ..base.ant_mind import AntMind
 class WorkerAntMind(AntMind):
 
     def _generate_thoughts(self):
-        super()._generate_thoughts()
-        if not self._has_thoughts_to_do():
-            thought = self._thought_factory.build_collect_food_thought(self._home_nest, self._memory)
-            self._register_thought(thought)
+        pass
+        # super()._generate_thoughts()
+        # if not self._has_thoughts_to_do():
+        #     thought = self._thought_factory.build_collect_food_thought(self._home_nest, self._memory)
+        #     self._register_thought(thought)
 
     def _generate_feed_myself_thought(self):
         return self._thought_factory.build_feed_myself_thought(self._home_nest, self._memory)
