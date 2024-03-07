ad39618b110230cc5300c05965a5bd5414908952
Anton Devianin <adevianin@gmail.com>
2024-03-07 21:20:35 +0200

    fix

--- bugs/core/world/entities/nest/nest_factory.py
+++ bugs/core/world/entities/nest/nest_factory.py
@@ -13,7 +13,7 @@ class NestFactory():
         self._event_bus = event_bus
 
     def build_new_nest(self, id: int, position: Point, from_colony_id: int, owner_id: int):
-        return self.build_nest(id=id, position=position, angle=0, hp=100, from_colony_id=from_colony_id, owner_id=owner_id, larvae=[], eggs=[], larva_places_count=3, stored_calories=0, area=300, build_progress=0)
+        return self.build_nest(id=id, position=position, angle=0, hp=100, from_colony_id=from_colony_id, owner_id=owner_id, larvae=[], eggs=[], larva_places_count=3, egg_places_count=3, stored_calories=0, area=300, build_progress=0)
     
     def build_nest(self, id: int, position: Point, angle: int, from_colony_id: int, owner_id: int, hp: int, larvae: List[Larva], eggs: List[Egg], larva_places_count: int, egg_places_count: int, stored_calories: int, area: int, 
                    build_progress: int):
