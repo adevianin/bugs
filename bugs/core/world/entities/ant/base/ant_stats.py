d8dbcafa49ee82a182844d57c1df1d48d854874b
Anton Devianin <adevianin@gmail.com>
2024-10-14 17:47:56 +0300

    life span in stats

--- bugs/core/world/entities/ant/base/ant_stats.py
+++ bugs/core/world/entities/ant/base/ant_stats.py
@@ -5,18 +5,17 @@ from core.world.entities.ant.base.ant_types import AntTypes
 
 class AntStats(LiveStats):
 
-    NDIGITS = 0
-
     @classmethod
     def build(cls, ant_type: AntTypes, genome: Genome):
         phenotype = genome.generate_phenotype(ant_type)
-        max_hp = round(phenotype.max_hp, AntStats.NDIGITS)
-        hp_regen_rate = round(phenotype.hp_regen_rate, AntStats.NDIGITS)
-        speed = round(phenotype.speed, AntStats.NDIGITS)
-        sight_distance = round(phenotype.sight_distance, AntStats.NDIGITS)
-        strength = round(phenotype.strength, AntStats.NDIGITS)
-        defense = round(phenotype.defense, AntStats.NDIGITS)
-        appetite = round(phenotype.appetite, AntStats.NDIGITS)
-        min_temperature = round(phenotype.min_temperature, AntStats.NDIGITS)
-        return AntStats(StatsTypes.ANT, max_hp, hp_regen_rate, speed, sight_distance, strength, defense, appetite, min_temperature)
+        max_hp = round(phenotype.max_hp)
+        hp_regen_rate = round(phenotype.hp_regen_rate)
+        speed = round(phenotype.speed)
+        sight_distance = round(phenotype.sight_distance)
+        strength = round(phenotype.strength)
+        defense = round(phenotype.defense)
+        appetite = round(phenotype.appetite)
+        min_temperature = round(phenotype.min_temperature)
+        life_span = round(phenotype.life_span)
+        return AntStats(StatsTypes.ANT, max_hp, hp_regen_rate, speed, sight_distance, strength, defense, appetite, min_temperature, life_span)
     
--- bugs/core/world/entities/base/live_entity/live_stats.py
+++ bugs/core/world/entities/base/live_entity/live_stats.py
@@ -4,10 +4,11 @@ from core.world.entities.base.stats_types import StatsTypes
 class LiveStats(BasicStats):
 
     @classmethod
-    def build(cls, max_hp: int, hp_regen_rate: int, distance_per_step: int, sight_distance: int, attack: int, defence: int, appetite: int = 0, min_temperature: int = None):
-        return LiveStats(StatsTypes.LIVE, max_hp, hp_regen_rate, distance_per_step, sight_distance, attack, defence, appetite, min_temperature)
+    def build(cls, max_hp: int, hp_regen_rate: int, distance_per_step: int, sight_distance: int, attack: int, defence: int, appetite: int, min_temperature: int, life_span: int):
+        return LiveStats(StatsTypes.LIVE, max_hp, hp_regen_rate, distance_per_step, sight_distance, attack, defence, appetite, min_temperature, life_span)
 
-    def __init__(self, type: StatsTypes, max_hp: int, hp_regen_rate: int, distance_per_step: int, sight_distance: int, attack: int, defence: int, appetite: int, min_temperature: int):
+    def __init__(self, type: StatsTypes, max_hp: int, hp_regen_rate: int, distance_per_step: int, sight_distance: int, attack: int, defence: int, appetite: int, 
+                 min_temperature: int, life_span: int):
         super().__init__(type, max_hp, hp_regen_rate)
         self.distance_per_step = distance_per_step
         self.sight_distance = sight_distance
@@ -15,3 +16,4 @@ class LiveStats(BasicStats):
         self.defence = defence
         self.appetite = appetite
         self.min_temperature = min_temperature
+        self.life_span = life_span
