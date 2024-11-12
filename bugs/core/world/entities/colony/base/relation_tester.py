13faa27c8f966f87e49aa64a023a624fb76fbf94
Anton Devianin <adevianin@gmail.com>
2024-11-12 21:30:15 +0200

    fixed relation tester

--- bugs/core/world/entities/colony/base/relation_tester.py
+++ bugs/core/world/entities/colony/base/relation_tester.py
@@ -1,13 +1,15 @@
 from .colony_relations_table import ColonyRelationsTable
-from core.world.entities.base.entity import Entity
-
+from core.world.entities.base.entity_types import EntityTypesPack
+from core.world.entities.base.enemy_interface import iEnemy
 class RelationTester():
 
     def __init__(self, colony_relations_table: ColonyRelationsTable, colony_id: int):
         self._colony_relations_table = colony_relations_table
         self._colony_id = colony_id
 
-    def is_enemy(self, entity: Entity):
+    def is_enemy(self, entity: iEnemy):
+        if entity.type not in EntityTypesPack.LIVE_ENTITIES:
+            raise Exception('enemy can be only live entity')
         if entity.from_colony_id == self._colony_id: 
             return False
         return self._colony_relations_table.get_relation_value(entity.from_colony_id, self._colony_id) < 0
