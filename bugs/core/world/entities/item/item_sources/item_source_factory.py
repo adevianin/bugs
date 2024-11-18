e0876870f0f4998b81bdfb1b0a4bde0ffe900700
Anton Devianin <adevianin@gmail.com>
2024-11-18 22:36:26 +0200

    Revert "refact item source"
    This reverts commit 1a90cc83437d43de16cf2fa510f645f2bd4a227d.

--- bugs/core/world/entities/item/item_sources/item_source_factory.py
+++ bugs/core/world/entities/item/item_sources/item_source_factory.py
@@ -1,10 +1,10 @@
 from core.world.utils.event_emiter import EventEmitter
 from core.world.utils.point import Point
 from core.world.entities.item.items.base.item_types import ItemTypes
+from core.world.entities.item.item_sources.honeydew_item_source.honeydew_item_source import HoneydewItemSource
+from core.world.entities.item.item_sources.honeydew_item_source.honeydew_item_source_body import HoneydewItemSourceBody
 from core.world.entities.base.stats_library import StatsLibrary
 from core.world.entities.base.ownership_config import OwnershipConfig
-from .base.item_source_body import ItemSourceBody
-from .base.item_source import ItemSource
 from core.world.entities.world.season_types import SeasonTypes
 
 class ItemSourceFactory():
@@ -12,14 +12,16 @@ class ItemSourceFactory():
     def __init__(self, event_bus: EventEmitter):
         self._event_bus = event_bus
 
-    def build_new_item_source(self, id: int, position: Point, item_type: ItemTypes, fertility: int, min_item_strength: int, max_item_strength: int, current_season: SeasonTypes):
-        ownership = OwnershipConfig.build_empty()
-        hp = StatsLibrary.ITEM_SOURCE_DEFAULT.max_hp
-        is_active = ItemSource.check_is_fertile_season_for_item_type(current_season, item_type)
-        return self.build_item_source(id, ownership, hp, position, 0, item_type, fertility, 0, min_item_strength, max_item_strength, is_active)
+    def build_item_source(self, id: int, ownership: OwnershipConfig, hp: int, position: Point, angle: int, item_type: ItemTypes, 
+                          fertility: int, accumulated: int, min_item_strength: int, max_item_strength: int):
+        match(item_type):
+            case ItemTypes.HONEYDEW:
+                return self._build_honeydew_item_source(id, ownership, hp, position, angle, fertility, accumulated, min_item_strength, max_item_strength)
+            case _:
+                raise Exception('unknown type of item source')
 
-    def build_item_source(self, id: int, ownership: OwnershipConfig, hp: int, position: Point, angle: int, item_type: ItemTypes, fertility: int, accumulated: int, 
-                          min_item_strength: int, max_item_strength: int, is_active: bool):
+    def _build_honeydew_item_source(self, id: int, ownership: OwnershipConfig, hp: int, position: Point, angle: int, fertility: int, 
+                                    accumulated: int, min_item_strength: int, max_item_strength: int):
         stats = StatsLibrary.ITEM_SOURCE_DEFAULT
-        body = ItemSourceBody(EventEmitter(), stats, position, angle, hp)
-        return ItemSource(self._event_bus, EventEmitter(), id, ownership, body, item_type, fertility, accumulated, min_item_strength, max_item_strength, is_active)
+        body = HoneydewItemSourceBody(EventEmitter(), stats, position, angle, hp)
+        return HoneydewItemSource(self._event_bus, EventEmitter(), id, ownership, body, fertility, accumulated, min_item_strength, max_item_strength)
