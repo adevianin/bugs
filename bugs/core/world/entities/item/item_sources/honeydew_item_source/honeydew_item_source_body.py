68668f05e8c13247944eb54e9e91f576adf92315
Anton Devianin <adevianin@gmail.com>
2024-11-18 23:24:02 +0200

    added size to honydew body

--- bugs/core/world/entities/item/item_sources/honeydew_item_source/honeydew_item_source_body.py
+++ bugs/core/world/entities/item/item_sources/honeydew_item_source/honeydew_item_source_body.py
@@ -1,4 +1,5 @@
 from core.world.entities.item.item_sources.base.item_source_body import ItemSourceBody
+from core.world.utils.size import Size
 
 class HoneydewItemSourceBody(ItemSourceBody):
-    pass
+    SIZE = Size(90, 110)
