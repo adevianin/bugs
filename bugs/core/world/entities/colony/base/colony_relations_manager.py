88c03f1a8b32da4a5d55c3d9f6af82db6b8c4ed3
Anton Devianin <adevianin@gmail.com>
2024-11-21 18:22:54 +0200

    ladybug colony is ants enemy

--- bugs/core/world/entities/colony/base/colony_relations_manager.py
+++ bugs/core/world/entities/colony/base/colony_relations_manager.py
@@ -1,7 +1,7 @@
 from .colony_relations_table import ColonyRelationsTable
 from core.world.utils.event_emiter import EventEmitter
 from core.world.entities.colony.base.colony import Colony
-from core.world.settings import GROUND_BEETLE_COLONY_ID, OFFENSIVE_OPERATION_RELATION_COST
+from core.world.settings import ANT_COLONY_ENEMIES_IDS, OFFENSIVE_OPERATION_RELATION_COST
 
 class ColonyRelationsManager():
 
@@ -19,13 +19,14 @@ class ColonyRelationsManager():
         return self._relations_table
 
     def _on_colony_born(self, colony: Colony):
-        self._relations_table.set_relation_value(colony.id, GROUND_BEETLE_COLONY_ID, -1)
+        for id in ANT_COLONY_ENEMIES_IDS:
+            self._relations_table.set_relation_value(colony.id, id, -1)
 
     def _on_colony_died(self, colony: Colony):
         self._relations_table.clear_relations_for_colony(colony.id)
 
     def _on_step_done(self, step_number: int, season):
-        self._relations_table.improve_relations([GROUND_BEETLE_COLONY_ID])
+        self._relations_table.improve_relations(ANT_COLONY_ENEMIES_IDS)
 
     def _on_offensive_operation(self, attacking_colony_id: int, defending_colony_id: int):
         self._relations_table.set_relation_value(attacking_colony_id, defending_colony_id, -OFFENSIVE_OPERATION_RELATION_COST)
