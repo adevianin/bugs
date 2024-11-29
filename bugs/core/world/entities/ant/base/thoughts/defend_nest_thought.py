f3a717d0b0eafb941a80a5a12371c91d77015417
Anton Devianin <adevianin@gmail.com>
2024-11-29 20:44:07 +0200

    refact defend nest

--- bugs/core/world/entities/ant/base/thoughts/defend_nest_thought.py
+++ bugs/core/world/entities/ant/base/thoughts/defend_nest_thought.py
@@ -5,7 +5,6 @@ from core.world.entities.base.live_entity.thoughts.fight_near_enemies_thought im
 from core.world.entities.nest.nest import Nest
 from typing import List
 from core.world.utils.point import Point
-from core.world.entities.base.live_entity.thoughts.random_walk_thought import RandomWalkThought
 
 class DefendNestThought(Thought):
 
@@ -16,10 +15,6 @@ class DefendNestThought(Thought):
         self._nested_thoughts['fight_near_enemies_thought'] = fight_near_enemies_thought
         self._point_to_check = point_to_check
         self._defending_nest = defending_nest
-
-        self._defending_nest.events.add_listener('died', self._on_defending_nest_died)
-        self._defending_nest.events.add_listener('is_under_attack', self._on_defending_nest_id_under_attack)
-        self._defending_nest.events.add_listener('attack_is_over', self._on_defending_nest_attack_is_over)
 
     @property
     def fight_near_enemies_thought(self) -> FightNearEnemiesThought:
@@ -32,6 +27,18 @@ class DefendNestThought(Thought):
     @property
     def defending_nest_id(self):
         return self._defending_nest.id
+    
+    def _on_start_thinking(self):
+        super()._on_start_thinking()
+        self._defending_nest.events.add_listener('died', self._on_defending_nest_died)
+        self._defending_nest.events.add_listener('is_under_attack', self._on_defending_nest_id_under_attack)
+        self._defending_nest.events.add_listener('attack_is_over', self._on_defending_nest_attack_is_over)
+    
+    def _on_stop_thinking(self):
+        super()._on_stop_thinking()
+        self._defending_nest.events.remove_listener('died', self._on_defending_nest_died)
+        self._defending_nest.events.remove_listener('is_under_attack', self._on_defending_nest_id_under_attack)
+        self._defending_nest.events.remove_listener('attack_is_over', self._on_defending_nest_attack_is_over)
     
     def do_step(self):
         self.fight_near_enemies_thought.do_step()
@@ -55,11 +62,6 @@ class DefendNestThought(Thought):
     def _on_defending_nest_died(self):
         self.cancel()
 
-    def _on_stop_thinking(self):
-        super()._on_stop_thinking()
-
-        self._defending_nest.events.remove_listener('died', self._on_defending_nest_died)
-        self._defending_nest.events.remove_listener('is_under_attack', self._on_defending_nest_id_under_attack)
-        self._defending_nest.events.remove_listener('attack_is_over', self._on_defending_nest_attack_is_over)
+    
 
         
