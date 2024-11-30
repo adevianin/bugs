2df85fbff90bd000b60ed18e67694c88f1cfeec6
Anton Devianin <adevianin@gmail.com>
2024-11-30 02:13:59 +0200

    build fortification operation thought refactored

--- bugs/core/world/entities/ant/base/thoughts/build_fortification_thought.py
+++ bugs/core/world/entities/ant/base/thoughts/build_fortification_thought.py
@@ -6,12 +6,20 @@ from core.world.entities.nest.nest import Nest
 
 class BuildFortificationThought(Thought):
 
+    class Flags(Thought.Flags):
+        FOUND_ITEM = 'found_item'
+        NEAR_ITEM = 'near_item'
+        PICKED_ITEM = 'picked_item'
+        NEAR_NEST = 'near_nest'
+
     _body: AntBody
 
     def __init__(self, random_walk_thought: RandomWalkThought, nest: Nest, flags: dict = None, sayback: str = None):
         super().__init__(type=ThoughtTypes.BUILD_FORTIFICATION, flags=flags, sayback=sayback)
         self._nested_thoughts['random_walk_thought'] = random_walk_thought
         self._nest = nest
+
+        self._nest_block_removal_id = self._nest.block_removal()
 
     @property
     def nest_id(self):
@@ -21,23 +29,27 @@ class BuildFortificationThought(Thought):
     def random_walk_thought(self) -> RandomWalkThought:
         return self._nested_thoughts['random_walk_thought']
     
+    def _on_stop_thinking(self):
+        super()._on_stop_thinking()
+        self._nest.unblock_removal(self._nest_block_removal_id)
+    
     def cancel(self):
         super().cancel()
         if self._body.is_item_picked:
             self._body.drop_picked_item()
 
     def do_step(self):
-        if not self._read_flag('found_item'):
+        if not self._read_flag(self.Flags.FOUND_ITEM):
             items = self._body.look_around_for_building_items()
             if len(items) > 0:
                 self._body.remember_entity('building_item', items[0])
-                self._write_flag('found_item', True)
+                self._write_flag(self.Flags.FOUND_ITEM, True)
                 return
             
             self.random_walk_thought.do_step()
             return
 
-        if self._read_flag('found_item') and not self._read_flag('near_item'):
+        if self._read_flag(self.Flags.FOUND_ITEM) and not self._read_flag(self.Flags.NEAR_ITEM):
             item_data = self._body.recall_entity('building_item')
             item = self._body.look_around_for_item_with_id(item_data['id'])
             if not item:
@@ -45,27 +57,27 @@ class BuildFortificationThought(Thought):
                 return
             is_walk_done = self._body.step_to(item_data['position'])
             if is_walk_done:
-                self._write_flag('near_item', True)
+                self._write_flag(self.Flags.NEAR_ITEM, True)
             return
             
-        if self._read_flag('near_item') and not self._read_flag('picked_item'):
+        if self._read_flag(self.Flags.NEAR_ITEM) and not self._read_flag(self.Flags.PICKED_ITEM):
             item_data = self._body.recall_entity('building_item')
             item = self._body.look_around_for_item_with_id(item_data['id'])
             if item:
                 self._body.pick_up_item(item)
-                self._write_flag('picked_item', True)
+                self._write_flag(self.Flags.PICKED_ITEM, True)
             else:
                 self._forget_found_item()
 
             return
         
-        if self._read_flag('picked_item') and not self._read_flag('near_nest'):
+        if self._read_flag(self.Flags.PICKED_ITEM) and not self._read_flag(self.Flags.NEAR_NEST):
             is_walk_done = self._body.step_to(self._nest.position)
             if is_walk_done:
-                self._write_flag('near_nest', True)
+                self._write_flag(self.Flags.NEAR_NEST, True)
             return
 
-        if self._read_flag('near_nest'):
+        if self._read_flag(self.Flags.NEAR_NEST):
             if not self._nest.is_died:
                 self._body.give_fortificating_item(self._nest)
             else:
@@ -74,6 +86,6 @@ class BuildFortificationThought(Thought):
 
     def _forget_found_item(self):
         self._body.forget_entity('building_item')
-        self._write_flag('found_item', False)
-        self._write_flag('near_item', False)
+        self._write_flag(self.Flags.FOUND_ITEM, False)
+        self._write_flag(self.Flags.NEAR_ITEM, False)
     
