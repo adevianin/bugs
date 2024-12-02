36556a9ccc3c3c4d81d3519315d509800a04016c
Anton Devianin <adevianin@gmail.com>
2024-12-03 00:58:53 +0200

    fix

--- bugs/core/start.py
+++ bugs/core/start.py
@@ -234,7 +234,7 @@ def start():
     world_service.set_world(world_facade.world)
 
     # MY_TEST_ENV['attacker'] = world_facade.world.map.get_entity_by_id(5)
-    MY_TEST_ENV['attacker2'] = world_facade.world.map.get_entity_by_id(6)
+    # MY_TEST_ENV['attacker2'] = world_facade.world.map.get_entity_by_id(6)
     # MY_TEST_ENV['attacker3'] = world_facade.world.map.get_entity_by_id(7)
 
     world_facade.world.run()
