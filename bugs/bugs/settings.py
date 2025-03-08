429104c4baf8e51866bd464c26b838726c8018fd
Anton Devianin <adevianin@gmail.com>
2025-03-08 14:18:56 +0200

    added some translations to account page

--- bugs/bugs/settings.py
+++ bugs/bugs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,13 +123,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGE_CODE = 'uk'
+USE_I18N = True
+USE_L10N = True
+LOCALE_PATHS = [
+    BASE_DIR / 'locale'
+]
+LANGUAGES = [
+    ('en', _('English')),
+    ('uk', _('Українська')),
+]
 
 
 # Static files (CSS, JavaScript, Images)
