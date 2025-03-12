cb53539cc39889045d10c6b7eb036d830fc37390
Anton Devianin <adevianin@gmail.com>
2025-03-12 23:05:29 +0200

    refact views

--- bugs/infrastructure/urls.py
+++ bugs/infrastructure/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
     path('password_reset/', auth_views.PasswordResetView.as_view(
         template_name='client/password_reset/form.html', 
-        email_template_name='client/password_reset/email.html'
+        email_template_name='client/emails/password_reset.html'
         ), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='client/password_reset/done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='client/password_reset/confirm.html'), name='password_reset_confirm'),
