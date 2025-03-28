42f0c90058c67239c3d2c080c99f8a8b7208aad2
Anton Devianin <adevianin@gmail.com>
2025-03-28 23:22:32 +0200

    tree opacity on view

--- bugs/client/gameApp/src/view/world/entitiesViews/treeView.js
+++ bugs/client/gameApp/src/view/world/entitiesViews/treeView.js
@@ -5,11 +5,11 @@ class TreeView extends EntityView {
 
     constructor(entity, entityContainer) {
         super(entity, entityContainer);
+        this._treeRect = null
 
         this._render();
 
-        this._bodyContainer.on('mouseover', this._onMouseover.bind(this));
-        this._bodyContainer.on('mouseout', this._onMouseout.bind(this));
+        this.$eventBus.on('viewPointChanged', this._onViewPointChange.bind(this));
     }
 
     _render() {
@@ -20,23 +20,38 @@ class TreeView extends EntityView {
         this._sprite = new PIXI.Sprite(this.$textureManager.getTexture(`tree_summer.png`));
         this._bodyContainer.addChild(this._sprite);
 
-        // this._entityContainer.x = this._entity.position.x;
-        // this._entityContainer.y = this._entity.position.y;
-
         this._entityContainer.pivot.x = this._sprite.width / 2;
         this._entityContainer.pivot.y = this._sprite.height;
 
         this._bodyContainer.alpha = 0.5;
         this._renderEntityState();
+
+        this._treeRect = new PIXI.Rectangle(
+            this._entity.position.x - this._sprite.width / 2,
+            this._entity.position.y - this._sprite.height,
+            this._sprite.width,
+            this._sprite.height
+        )
+
+        // this._renderTreeRectDebug();
     }
 
-    _onMouseover() {
-        this._bodyContainer.alpha = 0.5;
+    _renderTreeRectDebug() {
+        let graphics = new PIXI.Graphics();
+        graphics.rect(this._treeRect.x, this._treeRect.y, this._treeRect.width, this._treeRect.height);
+        graphics.stroke({
+            color: 'red',
+            alpha: 0.5
+        });
+        this._parentContainer.addChild(graphics);
     }
 
-    _onMouseout() {
-        this._bodyContainer.alpha = 1;
+    _onViewPointChange(viewPoint, viewRect) {
+        if (this._isCurrentChunkVisible) {
+            this._bodyContainer.alpha = this._treeRect.contains(viewPoint.x, viewPoint.y) ? 0.5 : 1;
+        }
     }
+
 }
 
 export {
