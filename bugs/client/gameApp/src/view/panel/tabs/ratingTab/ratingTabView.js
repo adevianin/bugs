22bf820cb2366245605beb741b119dbbe3b01b80
Anton Devianin <adevianin@gmail.com>
2025-04-03 16:13:21 +0300

    pagination rating tab

--- bugs/client/gameApp/src/view/panel/tabs/ratingTab/ratingTabView.js
+++ bugs/client/gameApp/src/view/panel/tabs/ratingTab/ratingTabView.js
@@ -8,10 +8,14 @@ class RatingTabView extends BaseGameHTMLView {
     constructor(el) {
         super(el);
         this._ratingContainer = this.$domain.ratingContainer;
-
-        this._ratingContainer.on('changed', this._onRatingChanged.bind(this));
+        this._currentPortionIndex = 0;
+        this._portionSize = 10;
 
         this._render();
+
+        this._ratingContainer.on('changed', this._onRatingChanged.bind(this));
+        this._prevBtn.addEventListener('click', this._onPrevBtnClick.bind(this));
+        this._nextBtn.addEventListener('click', this._onNextBtnClick.bind(this));
     }
 
     _render() {
@@ -19,12 +23,25 @@ class RatingTabView extends BaseGameHTMLView {
 
         this._ratingPlacesListEl = this._el.querySelector('[data-rating-places-list]');
 
-        for (let ratingPlace of this._ratingContainer.ratingPlaces) {
-            this._renderRatinPlace(ratingPlace);
+        this._prevBtn = this._el.querySelector('[data-prev]');
+        this._nextBtn = this._el.querySelector('[data-next]');
+
+        this._renderCurrentPortion();
+        this._renderBtnsState();
+    }
+
+    _renderCurrentPortion() {
+        this._ratingPlacesListEl.innerHTML = '';
+        let startIndex = this._currentPortionIndex * this._portionSize;
+        let endIndex = startIndex + this._portionSize;
+
+        let places = this._ratingContainer.ratingPlaces.slice(startIndex, endIndex);
+        for (let place of places) {
+            this._renderRatingPlace(place);
         }
     }
 
-    _renderRatinPlace(ratingPlace) {
+    _renderRatingPlace(ratingPlace) {
         let tr = document.createElement('tr');
         tr.innerHTML = ratingPlaceTmpl;
         tr.querySelector('[data-place-number]').innerHTML = ratingPlace.place;
@@ -32,6 +49,23 @@ class RatingTabView extends BaseGameHTMLView {
         tr.querySelector('[data-ants-count]').innerHTML = ratingPlace.ants;
         tr.querySelector('[data-colonies-count]').innerHTML = ratingPlace.colonies;
         this._ratingPlacesListEl.append(tr);
+    }
+
+    _renderBtnsState() {
+        this._prevBtn.disabled = this._currentPortionIndex == 0;
+        this._nextBtn.disabled = (this._currentPortionIndex + 1) * this._portionSize >= this._ratingContainer.ratingPlaces.length
+    }
+
+    _onPrevBtnClick() {
+        this._currentPortionIndex--;
+        this._renderCurrentPortion();
+        this._renderBtnsState();
+    }
+
+    _onNextBtnClick() {
+        this._currentPortionIndex++;
+        this._renderCurrentPortion();
+        this._renderBtnsState();
     }
 
     _onRatingChanged() {
