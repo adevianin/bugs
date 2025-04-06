14e2d94ca363d5132c9efab3e86a59dcd4ab98a5
Anton Devianin <adevianin@gmail.com>
2025-04-06 12:41:18 +0300

    added abstractmethod to vision stream interface

--- bugs/core/world/entities/base/live_entity/vision_stream_interface.py
+++ bugs/core/world/entities/base/live_entity/vision_stream_interface.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from core.world.utils.point import Point
 from typing import List, Callable
 from core.world.entities.base.entity_types import EntityTypes
@@ -6,5 +6,6 @@ from core.world.entities.base.entity import Entity
 
 class iVisionStream(ABC):
 
+    @abstractmethod
     def find_entities_near(self, point: Point, max_distance: int, entity_types: List[EntityTypes] = None, filter: Callable[[Entity], bool] = None, is_detectable_only: bool = True) -> List[Entity]:
         pass
