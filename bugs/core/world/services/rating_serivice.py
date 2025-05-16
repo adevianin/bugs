540aa59291cf3d7eaada580d6dfef4951d531c85
Anton Devianin <adevianin@gmail.com>
2025-05-16 16:57:39 +0300

    fixed rating

--- bugs/core/world/services/rating_serivice.py
+++ bugs/core/world/services/rating_serivice.py
@@ -2,7 +2,9 @@ from .base_service import BaseService
 from core.world.entities.world.player_stats import PlayerStats
 from core.world.entities.action.rating_updated_action import RatingUpdatedAction
 from core.world.entities.world.player_stats_factory import PlayerStatsFactory
-from typing import List
+from core.world.entities.ant.base.ant import Ant
+from core.world.entities.base.entity_types import EntityTypes
+from typing import Callable
 
 class RatingService(BaseService):
 
@@ -45,7 +47,9 @@ class RatingService(BaseService):
     def _get_or_create_player_stats(self, owner_id: int) -> PlayerStats:
         player_stats = self._get_player_stats_for_owner(owner_id)
         if not player_stats:
-            player_stats = self._player_stats_factory.build_player_stats(owner_id, 0, 0)
+            ants_count = self._count_player_ants(owner_id)
+            colonies_count = self._count_player_colonies(owner_id)
+            player_stats = self._player_stats_factory.build_player_stats(owner_id, ants_count, colonies_count)
             self._world.add_new_player_stats(player_stats)
 
         return player_stats
@@ -56,4 +60,18 @@ class RatingService(BaseService):
                 return player_stats
             
         return None
+    
+    def _count_player_ants(self, player_id: int):
+        owner_filter: Callable[[Ant], bool] = lambda ant: ant.owner_id == player_id
+        player_ants = self._world.map.get_entities(entity_types=[EntityTypes.ANT], filter=owner_filter)
+        return len(player_ants)
+    
+    def _count_player_colonies(self, player_id: int):
+        count = 0
+        for colony in self._world.ant_colonies:
+            if (colony.owner_id == player_id):
+                count += 1
+        return count
+    
+
         
