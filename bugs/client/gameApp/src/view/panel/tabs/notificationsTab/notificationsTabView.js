9c122a1e9a93b01537f092a5e6480a64ec00547e
Anton Devianin <adevianin@gmail.com>
2025-05-24 10:22:54 +0300

    styled notifications tab

--- bugs/client/gameApp/src/view/panel/tabs/notificationsTab/notificationsTabView.js
+++ bugs/client/gameApp/src/view/panel/tabs/notificationsTab/notificationsTabView.js
@@ -3,6 +3,7 @@ import { BaseGameHTMLView } from '@view/base/baseGameHTMLView';
 import notificationsTabTmpl from './notificationsTabTmpl.html';
 import { NotificationsListView } from "./notificationsListView";
 import { GAME_MESSAGE_IDS } from '@messages/messageIds';
+import { PanelTabHeadView } from '@view/panel/panelTabHead/panelTabHeadView';
 
 class NotificationsTabView extends BaseGameHTMLView {
 
@@ -16,7 +17,9 @@ class NotificationsTabView extends BaseGameHTMLView {
         this._el.innerHTML = notificationsTabTmpl;
         
         this._notificationsListView = new NotificationsListView(this._el.querySelector('[data-notifications]'));
-        this._el.querySelector('[data-tab-title]').innerHTML = this.$mm.get(GAME_MESSAGE_IDS.NOTIFICATIONS_TAB_TITLE);
+
+        let tabName = this.$mm.get(GAME_MESSAGE_IDS.NOTIFICATIONS_TAB_TITLE);
+        this._tabHeadView = new PanelTabHeadView(this._el.querySelector('[data-tab-head]'), tabName);
     }
 
 }
