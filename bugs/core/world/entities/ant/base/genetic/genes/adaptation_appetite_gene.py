77b1a54e7a99974c6bb955c813deb9fd22467e5d
Anton Devianin <adevianin@gmail.com>
2025-06-05 10:37:05 +0300

    balancing appetite

--- bugs/core/world/entities/ant/base/genetic/genes/adaptation_appetite_gene.py
+++ bugs/core/world/entities/ant/base/genetic/genes/adaptation_appetite_gene.py
@@ -3,7 +3,7 @@ from .base.base_gene import BaseGene
 from core.world.entities.ant.base.genetic.phenotype import Phenotype
 from .base.domination_codes import DominationCodes
 from core.world.entities.ant.base.genetic.chromosome_types import ChromosomeTypes
-from core.world.settings import BASE_ADAPTATION_APPETITE_MULTIPLIER
+from core.world.settings import BASE_ADAPTATION_APPETITE_MULTIPLIER, ANT_APPETITE_COMMON_MULTIPLIER
 
 class AdaptationAppetiteGene(BaseGene):
 
@@ -24,7 +24,8 @@ class AdaptationAppetiteGene(BaseGene):
         return self._multiplier
 
     def affect(self, phenotype: Phenotype):
-        phenotype.appetite = 0.25 * (phenotype.strength + phenotype.defense + phenotype.max_hp/10) * self._multiplier
+        common_mult = ANT_APPETITE_COMMON_MULTIPLIER
+        phenotype.appetite = common_mult * (phenotype.strength + phenotype.defense + phenotype.max_hp/10) * self._multiplier
 
     def merge(self, another_gene: 'AdaptationAppetiteGene') -> BaseGene:
         dominating_gene = super().merge(another_gene)
