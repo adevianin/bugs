669c4eb573168928db83f2bf71596a66f053308a
Anton Devianin <adevianin@gmail.com>
2025-06-22 13:20:45 +0300

    fixed webscoket url

--- bugs/infrastructure/views/game.py
+++ bugs/infrastructure/views/game.py
@@ -11,10 +11,15 @@ def index(request: HttpRequest):
     ef = EngineFacade.get_instance()
     if not ef.is_game_working:
         return render(request, 'client/maintenance.html')
+    
+    is_secure = request.is_secure()
+    ws_protocol = 'wss' if is_secure else 'ws'
+    host = request.get_host()
+    full_socket_url = f'{ws_protocol}://{host}{MAIN_SOCKET_URL}'
 
     initial_data = {
         'user': request.user.get_general_data(),
-        'mainSocketURL': MAIN_SOCKET_URL
+        'mainSocketURL': full_socket_url
     }
     
     return render(request, 'client/game.html', {'initial_data': initial_data})
