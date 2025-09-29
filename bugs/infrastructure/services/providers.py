bf8595059f70edfd8ce990c3a9fd175b965af326
Anton Devianin <adevianin@gmail.com>
2025-09-29 18:50:34 +0300

    used providers for email and account services

--- bugs/infrastructure/services/providers.py
+++ bugs/infrastructure/services/providers.py
@@ -1,22 +1,20 @@
 from .account_service import AccountService
 from .email_service import EmailService
 from .engine.engine_adapter import EngineAdapter
-from infrastructure.event_bus import event_bus
 
 _instances = {}
 
+def register_email_service(es: EmailService):
+    _instances['email_service'] = es
+
 def get_email_service() -> EmailService:
-    name = 'email_service'
-    if name not in _instances:
-        _instances[name] = EmailService()
-    return _instances[name]
+    return _instances['email_service']
+
+def register_account_service(account_service: AccountService):
+    _instances['account_service'] = account_service
 
 def get_account_service() -> AccountService:
-    name = 'account_service'
-    if name not in _instances:
-        email_service = get_email_service()
-        _instances[name] = AccountService(event_bus, email_service)
-    return _instances[name]
+    return _instances['account_service']
 
 def register_engine_adapter(ea: EngineAdapter):
     _instances['engine_adapter'] = ea
--- bugs/main/init.py
+++ bugs/main/init.py
@@ -3,7 +3,10 @@ from infrastructure.db.repositories.usernames_repository import UsernamesReposit
 from infrastructure.services.engine.engine_adapter import EngineAdapter
 import redis
 from decouple import config
-from infrastructure.services.providers import register_engine_adapter
+from infrastructure.event_bus import event_bus
+from infrastructure.services.providers import register_engine_adapter, register_account_service, register_email_service
+from infrastructure.services.email_service import EmailService
+from infrastructure.services.account_service import AccountService
 
 def init():
     world_data_repository = WorldDataRepository()
@@ -12,5 +15,9 @@ def init():
     r = redis.Redis(config('REDIS_HOST'), config('REDIS_PORT'), password=config('REDIS_PASSWORD'), decode_responses=True)
 
     ea = EngineAdapter(world_data_repository, usernames_repository, r)
+    email_service = EmailService()
+    account_service = AccountService(event_bus, email_service)
 
     register_engine_adapter(ea)
+    register_email_service(email_service)
+    register_account_service(account_service)
