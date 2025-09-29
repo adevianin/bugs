5d78f3bbf202f72820af67bc641c5d64e4ede0e5
Anton Devianin <adevianin@gmail.com>
2025-09-29 17:59:57 +0300

    commented not used views

--- bugs/infrastructure/views/nuptial.py
+++ bugs/infrastructure/views/nuptial.py
@@ -1,49 +1,49 @@
-from django.views.decorators.http import require_GET, require_POST
-from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponse
-from infrastructure.engine.engine_adapter import EngineAdapter
-from infrastructure.utils.clean_str_param import clean_str_param
+# from django.views.decorators.http import require_GET, require_POST
+# from django.contrib.auth.decorators import login_required
+# from django.http import HttpRequest, HttpResponse
+# from infrastructure.services.engine.engine_adapter import EngineAdapter
+# from infrastructure.utils.clean_str_param import clean_str_param
 
-import json
+# import json
 
-@require_POST
-@login_required     
-def found_colony(request: HttpRequest):
-    ea = EngineAdapter.get_instance()
+# @require_POST
+# @login_required     
+# def found_colony(request: HttpRequest):
+#     ea = EngineAdapter.get_instance()
 
-    try:
-        data = json.loads(request.body)
-        queen_id = int(data['queen_id'])
-        nuptial_male_id = int(data['nuptial_male_id'])
-        nest_building_site = data['nest_building_site']
-        colony_name = clean_str_param(data['colony_name'])
-    except Exception as e:
-        return HttpResponse(status=400)
+#     try:
+#         data = json.loads(request.body)
+#         queen_id = int(data['queen_id'])
+#         nuptial_male_id = int(data['nuptial_male_id'])
+#         nest_building_site = data['nest_building_site']
+#         colony_name = clean_str_param(data['colony_name'])
+#     except Exception as e:
+#         return HttpResponse(status=400)
 
-    ea.found_colony_command(request.user.id, queen_id, nuptial_male_id, nest_building_site, colony_name)
+#     ea.found_colony_command(request.user.id, queen_id, nuptial_male_id, nest_building_site, colony_name)
 
-    return HttpResponse(status=204)
+#     return HttpResponse(status=204)
 
-@require_POST
-@login_required     
-def save_specie_schema(request: HttpRequest):
-    ea = EngineAdapter.get_instance()
+# @require_POST
+# @login_required     
+# def save_specie_schema(request: HttpRequest):
+#     ea = EngineAdapter.get_instance()
 
-    try:
-        data = json.loads(request.body)
-        specie_schema = data['specie_schema']
-    except Exception as e:
-        return HttpResponse(status=400)
+#     try:
+#         data = json.loads(request.body)
+#         specie_schema = data['specie_schema']
+#     except Exception as e:
+#         return HttpResponse(status=400)
     
-    ea.change_specie_schema_command(request.user.id, specie_schema)
+#     ea.change_specie_schema_command(request.user.id, specie_schema)
 
-    return HttpResponse(status=200)
+#     return HttpResponse(status=200)
     
-@require_POST
-@login_required     
-def born_new_antara(request: HttpRequest):
-    ea = EngineAdapter.get_instance()
+# @require_POST
+# @login_required     
+# def born_new_antara(request: HttpRequest):
+#     ea = EngineAdapter.get_instance()
 
-    ea.born_new_antara_command(request.user.id)
+#     ea.born_new_antara_command(request.user.id)
 
-    return HttpResponse(status=200)
+#     return HttpResponse(status=200)
