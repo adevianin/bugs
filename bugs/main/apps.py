2b2ba4922a8f2ed8ceb50b3db60d24c572f3e550
Anton Devianin <adevianin@gmail.com>
2025-09-30 23:49:48 +0300

    disabled init for commands

--- bugs/main/apps.py
+++ bugs/main/apps.py
@@ -1,10 +1,21 @@
 from django.apps import AppConfig
+import sys
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 
     def ready(self):
+
+        ignored_commands = [
+            'makemigrations', 
+            'migrate', 
+            'build_frontend'
+        ]
+
+        if len(sys.argv) > 1 and sys.argv[1] in ignored_commands:
+            return
+        
         from .init import init
         init()
             
