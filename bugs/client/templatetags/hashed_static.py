a5df1155af14ff318c7f344608e15c3dca30e8e6
Anton Devianin <adevianin@gmail.com>
2025-10-09 18:31:39 +0300

    refact using settings

--- bugs/client/templatetags/hashed_static.py
+++ bugs/client/templatetags/hashed_static.py
@@ -1,7 +1,7 @@
 import json
 import os
 from django import template
-from django.conf import settings
+from bugs.settings import STATIC_ROOT, STATIC_URL
 
 register = template.Library()
 
@@ -11,8 +11,8 @@ _manifest_cache = None
 def hashed_static(path):
     global _manifest_cache
     if _manifest_cache is None:
-        manifest_path = os.path.join(settings.STATIC_ROOT, 'manifest.json')
+        manifest_path = os.path.join(STATIC_ROOT, 'manifest.json')
         with open(manifest_path, 'r') as f:
             _manifest_cache = json.load(f)
 
-    return settings.STATIC_URL + _manifest_cache.get(path, path)
+    return STATIC_URL + _manifest_cache.get(path, path)
