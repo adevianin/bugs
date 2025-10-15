3abfded2003070e3e9aeac4f3793480db083b345
Anton Devianin <adevianin@gmail.com>
2025-10-15 21:33:15 +0300

    refactored accordingly to async orm django 5.2

--- bugs/infrastructure/db/repositories/usernames_repository.py
+++ bugs/infrastructure/db/repositories/usernames_repository.py
@@ -1,13 +1,11 @@
 from infrastructure.services.engine.usernames_repository_interface import iUsernamesRepository
 from infrastructure.models import User
 from typing import Dict
-from asgiref.sync import sync_to_async
 
 class UsernamesRepository(iUsernamesRepository):
 
     async def get_usernames(self) -> Dict[int, str]:
-        return await self._get_usernames_sync()
+        queryset = User.objects.all().values('id', 'username')
+        usernames = [row async for row in queryset]
+        return usernames
     
-    @sync_to_async
-    def _get_usernames_sync(self):
-        return list(User.objects.values('id', 'username'))
